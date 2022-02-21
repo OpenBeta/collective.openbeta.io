2172ddc1ef8dbc83e3957a01d37c96d0aae8e0ba
Kevin Nadro <kevin.nadro@gmail.com>
2022-02-20 18:04:12 -0600

    mobile testimonal css

--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Testimonials() {
   return (
     <section className="flex bg-openbeta-dark-turquoise">
-      <div className='max-w-7xl mx-auto flex flex-row justify-between my-24 text-openbeta-white'>
+      <div className='max-w-7xl mx-auto flex xs:flex-col md:flex-row justify-between xs:pb-12 md:p-0 xs:mt-20 md:my-24 text-openbeta-white'>
         {quotes.map((entry, index) => (
           <Card key={index} {...entry} />
         ))}
@@ -12,12 +12,11 @@ export default function Testimonials() {
   );
 }
 
-const Card = ({ text, author, title }) => (
-  <div className="w-5/12">
+const Card = ({ text, author }) => (
+  <div className="md:w-5/12 xs:px-8 xs:pb-8 md:p-0">
     <q className="text-testimonials h-7 font-normal leading-8 font-open-sans">{text}</q>
     <div className="flex flex-col mt-12 items-end">
       <div className="text-2xl">— {author}</div>
-      <div className="text-2xl">{title}</div>
     </div>
   </div>
 );
@@ -25,12 +24,10 @@ const Card = ({ text, author, title }) => (
 const quotes = [
   {
     text: 'Just as climbing should be open to everyone. The knowledge of these great experiences should be open to anyone willing to learn.',
-    author: 'Brody Lowe',
-    title: 'title'
+    author: 'Brody Lowe'
   },
   {
     text: 'OpenBeta summed up my thoughts extremely well. I believe the data that was curated by the community should stay open to community use.',
-    author: 'Raymond Fiscus',
-    title: 'title'
+    author: 'Raymond Fiscus'
   },
 ];
