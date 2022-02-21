989b5f3f761448d222704d1a05a97bd945550b5d
Kevin Nadro <kevin.nadro@gmail.com>
2022-02-20 21:38:24 -0600

    making progress on blog article section on mobile

--- src/components/sections/LatestBlogArticle.js
+++ src/components/sections/LatestBlogArticle.js
@@ -5,8 +5,8 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 export default function LatestBlogArticle() {
   return (
-    <section className="flex bg-openbeta-white border-t border-black flex flex-col justify-center">
-      <div className='flex flex-row text-openbeta-black justify-between items-center'>
+    <section className="flex bg-openbeta-white md:border-t border-black flex flex-col justify-center">
+      <div className='flex-row text-openbeta-black justify-between items-center xs:hidden md:flex'>
         <StaticImage
           className="w-1/2"
           src="../../assets/heros/article-image.png"
@@ -25,6 +25,48 @@ export default function LatestBlogArticle() {
           Read more
           </Link>  
         </div>
+      </div>
+
+      {/* Mobile only */}
+      <div className="flex-row text-openbeta-black justify-between items-center md:hidden">
+      <div className="" style={{ display: 'grid' }}>
+        <StaticImage
+          className="z-0"
+          src="../../assets/heros/article-image.png"
+          alt=""
+          layout="fullWidth"
+          placeholder="blurred"
+          quality="80"
+          style={{
+            gridArea: '1/1',
+          }}
+        />
+        <div
+          style={{
+            // By using the same grid area for both, they are stacked on top of each other
+            gridArea: '1/1',
+            position: 'relative',
+            // This centers the other elements inside the hero component
+            placeItems: 'center',
+            display: 'grid',
+          }}
+        >
+          <div className="max-w-7xl text-white text-center px-8 pt-20 pb-6 bg-openbeta-dark-turquoise-opactiy">
+            <h3 className="font-bold text-3xl">
+              Latest blog article
+            </h3>
+            <h1 className="font-open-sans text-5xl py-8">Estimating Route Quality: Analysis and Applications</h1>
+            <div class="py-8">
+              <Link
+                  className="self-start py-6 px-12 btn btn-link text-center leading-10 bg-openbeta-orange xs:font-medium md:font-normal xs:text-2xl md:text-lg"
+                  to="https://openbeta.substack.com/p/estimating-route-quality-analysis"
+                >
+                  Read more
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     </section>
   );
