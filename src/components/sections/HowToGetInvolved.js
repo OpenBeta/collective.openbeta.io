a2dc51dece07815a72828ae2a644a2262521ae3f
Kevin Nadro <kevin.nadro@gmail.com>
2022-02-20 18:12:16 -0600

    mobile css for how to get invovled

--- src/components/sections/HowToGetInvolved.js
+++ src/components/sections/HowToGetInvolved.js
@@ -4,13 +4,13 @@ import { Link } from 'gatsby';
 export default function HowToGetInvolved() {
   return (
     <section className="flex bg-openbeta-white">
-      <div className='max-w-7xl mx-auto flex flex-col items-center text-openbeta-black mb-20'>
+      <div className='max-w-7xl mx-auto flex flex-col items-center text-openbeta-black mb-20 xs:px-8 md:p-0'>
         <div className="justify-center mt-14 text-center">
-          <h2 className="text-5xl">How to get involved</h2>
-          <h3 className="text-3xl mt-4 font-open-sans"> OpenBeta is an open source project, and you can help!</h3>
+          <h2 className="text-5xl xs:text-left md:text-center">How to get involved</h2>
+          <h3 className="text-3xl mt-4  xs:text-left md:text-center font-open-sans"> OpenBeta is an open source project, and you can help!</h3>
         </div>
         <Link
-          className="btn btn-link w-48 h-14 mt-12 btn btn-primary text-center leading-10 bg-openbeta-green"
+          className="py-4 px-12 rounded text-openbeta-white text-center xs:font-medium md:font-normal xs:text-2xl md:text-lg leading-10 bg-openbeta-green"
           to="/join"
         >
           Join Today
